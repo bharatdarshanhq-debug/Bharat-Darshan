335536e5bab44d4acdec99b985f4cc9df87632a7
bharatdarshanhq-debug <bharatdarshan.hq@gmail.com>
2026-01-19 13:33:12 +0530

    Allow FRONTEND_URL in CORS

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,9 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: process.env.FRONTEND_URL 
+      ? [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:3000']
+      : ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true,
   })
 );
